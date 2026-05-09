587b3e4d63cee3a68d2ef808f21ab17f210fdf26
test <test@gimjeong-gyus-MacBook-Pro.local>
2026-05-09 14:58:48 +0900

    fix(api): itemId 와 bear 토큰 이름 중복 수정

--- src/utils/api.js
+++ src/utils/api.js
@@ -56,7 +56,9 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('accessToken')
   if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`
+    config.headers['Authorization'] = token.startsWith('Bearer ')
+      ? token
+      : `Bearer ${token}`
   }
   return config
 })
